602d445c1f71871a1677055ce6a822290013e0e8
mariam-attia20 <mariamattia94.75@gmail.com>
2026-07-22 09:12:04 +0300

    Add files via upload

---
+++ library.c/STD_TYPES.h
@@ -0,0 +1,20 @@
+#ifndef STD_TYPES
+#define STD_TYPES
+//  unsigned -> u
+//  signed -> s
+//  char -> 8bit
+// 8 bits
+typedef signed char s8;
+typedef unsigned char u8;
+ // 16 bits
+typedef signed short int  s16;
+typedef unsigned short int u16;
+
+//64 bit 
+typedef signed long long int  s64;
+typedef unsigned long long int  u64;
+
+//floating ploints
+ typedef double d64;
+ typedef float f32;
+ #endif
