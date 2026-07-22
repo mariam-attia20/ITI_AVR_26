1b9b4ec488ff206de2c33f8b66ca989f8f50ea6d
mariam attia <mariamattia94.75@gmail.com>
2026-07-22 06:03:03 +0300

    add linked list

---
+++ c_programming/C_project/clinic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+//choose between modes 
+ 
+
+    return 0 ;
+}
--- c_programming/session 7/linked_list.c
+++ c_programming/session 7/linked_list.c
@@ -1,24 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct Node_t{
+// Structural self-referential template layout
+typedef struct Node_t
+{
     int data;
-    struct Node_t* next ; //point on the next struct so his kind is the same as struct 
+    struct Node_t *next;
+} node;
 
-}node;
-node *head = NULL; //pointer to the  first element 
+// Initializing global base tracker frame mapping list head position
+node *head = NULL;
 
-void addfirst(int value){
-    node *ptr = (node*)malloc(sizeof(node)) ;
-     if(ptr == NULL){
-        printf("failed to allocate\n");
-        return 1 ;
-    }else{
-        ptr -> data = value;
-        
+// Insert a new node at the beginning of the list
+void addFirst(int value)
+{
+    node *ptr = (node *)malloc(sizeof(node));
+
+    if (ptr == NULL)
+    {
+        printf("Can't allocate memory\n");
+    }
+    else
+    {
+        ptr->data = value;
+
+        if (head == NULL)
+        {
+            ptr->next = NULL;
+        }
+        else
+        {
+            ptr->next = head;
+        }
+        head = ptr;
     }
 }
-void main (void){
 
+// Print all nodes currently linked within the chain sequence
+void displaylist()
+{
+    if (head != NULL)
+    {
+        node *temp = head;
+
+        while (temp != NULL)
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+    else
+    {
+        printf("Empty list\n");
+    }
+}
+
+// Insert a new node at the end of the list boundary line
+void addLast(int data)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+
+    if (newNode != NULL)
+    {
+        newNode->data = data;
+        newNode->next = NULL;
+
+        if (head != NULL)
+        {
+            node *temp = head;
+
+            // Traverse until reaching the final element node
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        else
+        {
+            head = newNode;
+        }
+    }
+    else
+    {
+        printf("Can't allocate memory\n");
+    }
+}
+
+int main()
+{
+    // Insertion sequences target front indices
+    addFirst(4);
+    addFirst(44);
+    addFirst(40);
+
+    // Insertion sequences target rear indices
+    addLast(1);
+    addLast(9);
+
+    // Render final verification snapshot maps inside console output profiles
+    printf("Linked List Elements: ");
+    displaylist();
+
+    return 0;
 }
