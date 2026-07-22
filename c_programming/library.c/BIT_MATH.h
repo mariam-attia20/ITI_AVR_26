2a654bece024feff78752d228bdbd3ed6dbf6355
mariam attia <mariamattia94.75@gmail.com>
2026-07-22 07:05:56 +0300

    modified bit math library

--- c_programming/library.c/BIT_MATH.h
+++ c_programming/library.c/BIT_MATH.h
@@ -5,7 +5,8 @@
 #define CLEAR_BIT(reg , bit)  reg = reg & ~(1<< bit)
 #define TOOGLE_BIT(reg , bit)  reg = reg ^ (1<<bit)
 #define GET_BIT(reg , bit )     reg =  1 & (reg>>bit)
-#define ROTATE_BIT_l(reg, bit , size)  reg = (reg<<bit)| (reg <<(size-bit))
-#define ROTATE_BIT_R(reg , bit , size)  reg = (reg >> bit) | (reg << (size-bit)) 
+#define SET_BYTE(reg , value)  reg = value 
+#define ROTATE_BIT_l(reg, bit , size)  reg = (unsigned char) (reg<<bit)| (reg <<(size-bit))
+#define ROTATE_BIT_R(reg , bit , size)  reg = (unsigned char) (reg >> bit) | (reg << (size-bit)) 
 
 #endif
